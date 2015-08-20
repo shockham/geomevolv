81ef8d58edb8e5cee5620dfd20fdeaffb1794c21
shockham <samuel.hockham@gmail.com>
2015-08-20 10:58:56 +0100

    using location.hostname for ws server

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,6 @@
 (function (){
 
-    var socket = new WebSocket("ws://127.0.0.1:2794", "rust-websocket");
+    var socket = new WebSocket("ws://" + location.hostname + ":2794", "rust-websocket");
 
     socket.onmessage = function (event) {
         eval(event.data);
